e002b8ced0122c9e1e81e30980dbfc6f321ab314
Carlos Vega <carlosvega3031@gmail.com>
2026-01-11 14:06:40 -0300

    login en funcionamiento y css ya aplicado

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone : true,
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  correo: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    console.log('Correo:', this.correo);
+    console.log('Password:', this.password);
+
+    // aquí luego conectarás con Node.js y veras que funcionara :V
+    alert('Login funcionando');
+  }
+
+  irRegistro() {
+    this.router.navigate(['/registro']);
+  }
 }
